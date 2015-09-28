1e33e9db6a2f7dadc3c837ee735013a6026f79a0
cmqdcyy <cuimengqiang2007@126.com>
2015-09-28 15:48:47 +0800

    FEA:1234

--- src/test/java/com/bj58/cmq/designpattern/test/package-info.java
+++ src/test/java/com/bj58/cmq/designpattern/test/package-info.java
@@ -1,15 +1,3 @@
-/**  
- * Copyright © 2015五八信息技术有限公司. All rights reserved.
- *
- * @Title: package-info.java
- * @Prject: com.bj58.cmq.designpattern
- * @Package: com.bj58.cmq.designpattern.test
- * @Description: TODO
- * @author: Administrator  
- * @date: 2015-9-28 下午3:40:37
- * @version: V1.0
- *
- */
 /**
  * @功能 
  * @ClassName: package-info
